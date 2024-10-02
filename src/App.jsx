f61c52210ab52db05b05bd4abe1c2d1db29f0678
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-02 22:53:10 +0530

    Added css upto img

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
+import './App.css'
 function App() {
   
   return (
    <>
    <div className="bmi-calculator">
-    <div className="box"></div>
+    <div className="box">
+      <div className="img1"></div>
+    </div>
     <div className="data">
       <h2>BMI Calculator</h2>
       <div className="input-container">
         <label htmlFor="height">Height (cm) :</label>
-        <input type="number" required id="height" value={45} />
+        <input type="number" required id="height"  />
       </div>
       <div className="input-container">
         <label htmlFor="weight">Weight (kg) :</label>
-        <input type="number" required id="weight" value={45} />
+        <input type="number" required id="weight"  />
       </div>
       <button>Calcaulate BMI</button>
       <div className="result">
