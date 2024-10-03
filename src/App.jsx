86d6fe0d3b0e683e31e1c51d7f064bb325a43554
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-03 11:37:50 +0530

    Added error Message , reset button & flaticon. Updated CSS .

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ function App() {
   const [weight, setWeight] = useState("")
   const [bmi, setBmi] = useState(null)
   const [status, setStatus] = useState(null);
+  const [error, setError] = useState("");
 
   const calculateBmi= ()=>{
-    if (height && weight){
+    // using regluar expression to test its an number
+    // together using an test fn 
+    const isValidHeight = /^\d+$/.test(height);
+    const isValidWeight = /^\d+$/.test(weight);
+
+    if (isValidHeight && isValidWeight){
       const heightInMeters = height/100;
       const bmi = weight / (heightInMeters**2)
       setBmi(bmi.toFixed(2));
@@ -16,11 +22,20 @@ function App() {
       else if (bmi >=18.5 && bmi <24.9) setStatus("Normal Weight");
       else if (bmi >=24.9 && bmi < 29.9)  setStatus("Overweight");
       else  setStatus("Obesity");
+      setError("");
       }
     else {
       setBmi(null);
       setStatus(null);
+      setError("Please enter an numeric values for height and weight.")
     }
+  }
+  const resetAll= ()=>  {
+    setHeight("");
+    setWeight("");
+    setBmi(null);
+    setStatus(null);
+    setError("");
   }
   return (
    <>
@@ -28,23 +43,26 @@ function App() {
     <div className="box"></div>
     <div className="data">
       <h2>BMI Calculator</h2>
+      {error && <div className="error">{error}</div>}
       <div className="input-container">
         <label htmlFor="height">Height (cm) :</label>
-        <input type="number" required id="height" value={height}
+        <input type="number" required id="height" 
+        value={height} placeholder='170'
         onChange={e=> setHeight(e.target.value)} />
       </div>
       <div className="input-container">
         <label htmlFor="weight">Weight (kg) :</label>
-        <input type="number" required id="weight" value={weight}
+        <input type="number" required id="weight" 
+        value={weight} placeholder='60'
         onChange={e=> setWeight(e.target.value)}/>
       </div>
       <button onClick={calculateBmi}>Calcaulate BMI</button>
+      <button onClick={resetAll}>Reset</button>
       {bmi && <div className="result">
         <p>Your BMI is : {bmi}</p>
         <p>Status : {status}</p>
       </div>
       }
-      
     </div>
    </div>
    </>
