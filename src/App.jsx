39a4c319ad61834a2593824d7dc52aaffe4c1f0d
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-03 10:19:44 +0530

    Added useStates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 function App() {
-  
+  const [height, setHeight] = useState("")
+  const [weight, setWeight] = useState("")
+  const [bmi, setBmi] = useState(null)
+  const [status, setStatus] = useState(null);
+
+  const calculateBmi= ()=>{
+    console.log(height,weight);
+  }
   return (
    <>
    <div className="bmi-calculator">
@@ -11,13 +19,15 @@ function App() {
       <h2>BMI Calculator</h2>
       <div className="input-container">
         <label htmlFor="height">Height (cm) :</label>
-        <input type="number" required id="height"  />
+        <input type="number" required id="height" value={height}
+        onChange={e=> setHeight(e.target.value)} />
       </div>
       <div className="input-container">
         <label htmlFor="weight">Weight (kg) :</label>
-        <input type="number" required id="weight"  />
+        <input type="number" required id="weight" value={weight}
+        onChange={e=> setWeight(e.target.value)}/>
       </div>
-      <button>Calcaulate BMI</button>
+      <button onClick={calculateBmi}>Calcaulate BMI</button>
       <div className="result">
         <p>Your BMI is : 28.8</p>
         <p>Status : Overweight</p>
