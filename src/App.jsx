df51cdc90d210f6c97a1db04fbfd896aac8bc85b
yuvarajpm1853 <yuvarajpm1853@gmail.com>
2024-10-03 10:49:49 +0530

    Added bmi logic and conditional result

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import { useState } from 'react'
 import './App.css'
 function App() {
+  // variables used in input values must be empty string . Not null
   const [height, setHeight] = useState("")
   const [weight, setWeight] = useState("")
   const [bmi, setBmi] = useState(null)
   const [status, setStatus] = useState(null);
 
   const calculateBmi= ()=>{
-    console.log(height,weight);
+    if (height && weight){
+      const heightInMeters = height/100;
+      const bmi = weight / (heightInMeters**2)
+      setBmi(bmi.toFixed(2));
+      if(bmi < 18.5)  setStatus("UnderWeight");
+      else if (bmi >=18.5 && bmi <24.9) setStatus("Normal Weight");
+      else if (bmi >=24.9 && bmi < 29.9)  setStatus("Overweight");
+      else  setStatus("Obesity");
+      }
+    else {
+      setBmi(null);
+      setStatus(null);
+    }
   }
   return (
    <>
    <div className="bmi-calculator">
-    <div className="box">
-      <div className="img1"></div>
-    </div>
+    <div className="box"></div>
     <div className="data">
       <h2>BMI Calculator</h2>
       <div className="input-container">
@@ -28,10 +39,12 @@ function App() {
         onChange={e=> setWeight(e.target.value)}/>
       </div>
       <button onClick={calculateBmi}>Calcaulate BMI</button>
-      <div className="result">
-        <p>Your BMI is : 28.8</p>
-        <p>Status : Overweight</p>
+      {bmi && <div className="result">
+        <p>Your BMI is : {bmi}</p>
+        <p>Status : {status}</p>
       </div>
+      }
+      
     </div>
    </div>
    </>
